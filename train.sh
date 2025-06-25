7f13bfceb1d785c266b3c2e5cb5df564b1f333ff
Zthink <103019073+timecoderr@users.noreply.github.com>
2025-06-25 22:59:08 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,1 @@
 
-python run.py --index nycbike --time 90 --interval 16  --lr 1e-3 --dropout 0.3
-python run.py --index nycbike --time 60 --interval 24  --lr 1e-3 --dropout 0.3
-python run.py --index nycbike --time 30 --interval 48  --lr 1e-3 --dropout 0.3
-python run.py --index nyctaxi --time 90 --interval 16  --lr 1e-3 --dropout 0.3
-python run.py --index nyctaxi --time 60 --interval 24  --lr 1e-3 --dropout 0.1
-python run.py --index nyctaxi --time 30 --interval 48  --lr 1e-3 --dropout 0.1
